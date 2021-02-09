ca3dc3be07cf4de79e0baa5665913c8c142039c1
Jamille Bittencourt <jamille.bittencourt@thoughtworks.com>
2021-02-09 10:45:51 -0300

    Added Menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,14 +1,24 @@
 package com.twu.biblioteca;
 
+import java.io.IOException;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
 
         System.out.println(bibliotecaApp.openLibrary());
+
+        System.out.println(bibliotecaApp.showMenu());
     }
 
-    public String openLibrary(){
+
+    public String openLibrary() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
+
+    public String showMenu() {
+        return "\n========== Menu ==========\n1. List of books";
+    }
+
 }
--- test/com/twu/biblioteca/BibliotecaTests.java
+++ test/com/twu/biblioteca/BibliotecaTests.java
@@ -3,6 +3,8 @@ package com.twu.biblioteca;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class BibliotecaTests {
 
     @Test
@@ -15,5 +17,17 @@ public class BibliotecaTests {
 
         //then
         Assert.assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", welcomeMessage);
+    }
+
+    @Test
+    public void shouldReturnMenuAfterWelcomeMessage() throws IOException {
+        //given
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+        //when
+        String menu = bibliotecaApp.showMenu();
+
+        //then
+        Assert.assertEquals("\n========== Menu ==========\n1. List of books", menu);
     }
 }
